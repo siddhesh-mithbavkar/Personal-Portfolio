a783879596739a99ed2b03d1b9f45c1e933a756f
siddhesh <siddheshmithbavkar98@gmail.com>
2026-01-22 22:10:21 +0530

    bug fixes

--- script.js
+++ script.js
@@ -34,15 +34,8 @@ $(document).ready(function(){
     });
 
     // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Frontend Developer","YouTuber", "Blogger", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Frontend Developer","YouTuber", "Blogger", "Freelancer"],
+var typed = new Typed(".typing", {
+        strings: ["Frontend Developer", "YouTuber", "Blogger", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
